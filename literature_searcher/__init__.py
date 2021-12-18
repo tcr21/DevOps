bd2d3f7fd6bc3e4987025fe4009bc6894ce1c975
oisdk <mail@doisinkidney.com>
2021-12-18 13:34:49 +0000

    Update __init__.py

--- literature_searcher/__init__.py
+++ literature_searcher/__init__.py
@@ -19,10 +19,7 @@ def create_app(test_config=None):
         app.config.from_mapping(test_config)
 
     # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
+    os.makedirs(app.instance_path, exist_ok=True)
 
     @app.route("/")
     def home():

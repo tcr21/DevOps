84c86f441b68a260748ee84aefd3e27415cdfb56
Jonas Birk <jonasbirk1996@gmail.com>
2022-01-24 20:39:51 +0000

    Making download file type dynamic depending on home page selection

--- literature_searcher/search_result.py
+++ literature_searcher/search_result.py
@@ -11,21 +11,23 @@ def response():
     query = request.args["search_query"]
     file_type = request.args["file_type"]
 
-    if file_type == 'pdf':
-        file_name = 'literature_searcher/results/result.md'
-        args = ['pandoc', 'literature_searcher/results/result.md', '-o', 'literature_searcher/results/result.pdf', "--pdf-engine=pdflatex"]
-        subprocess.run(args)
-    else:
-        file_name = 'literature_searcher/results/result.' + file_type
-
     if not query:
         flash("No query")
     search_results = query_processor.process(query)
+
+    file_path = 'literature_searcher/results/result.'
+    file_extension = 'md' if file_type == 'pdf' else file_type
+    file_name = file_path + file_extension
+
     f = open(file_name, 'w')
     f.write(search_results[0])
     f.close()
-    return render_template("search_result.html", search_results=search_results)
 
+    if file_type == 'pdf':
+        args = ['pandoc', 'literature_searcher/results/result.md', '-o', 'literature_searcher/results/result.pdf', "--pdf-engine=pdflatex"]
+        subprocess.run(args)
+  
+    return render_template("search_result.html", search_results=search_results, file_type=file_type)
 
 @bp.route("/download/<filetype>")
 def download(filetype):
